c1a1dc12d38e7cc2f513dc567ee52263f40e3831
apupple <kyleeannreid@gmail.com>
2026-04-10 14:05:04 -0500

    Update 03_queries.sql
    Added a query for contact info, reformatted title to match other files

--- 03_queries.sql
+++ 03_queries.sql
@@ -1,5 +1,9 @@
-==========================
--- QUERIES for P.A.W.S.
-==========================
--- 1) Total Animals Adopted --
+-- ==========================
+-- P.A.W.S QUERIES --
+-- ==========================
+-- 1) Display Contact Info of All Persons --
+
+SELECT f_get_full_name(person_first_name, person_last_name)
+FROM PAWS.person;
+
 -- 2) Min/Max --
