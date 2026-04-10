6268b06e6a6cf475dfb786ad25261cc4d0ccba86
apupple <kyleeannreid@gmail.com>
2026-04-10 14:02:27 -0500

    Update 04_triggers_procedure_function.sql
    New f_get_full_name() function

--- 04_triggers_procedure_function.sql
+++ 04_triggers_procedure_function.sql
@@ -1,1 +1,19 @@
+-- ===============================
+-- P.A.W.S. TRIGGERS, PROCEDURES, FUNCTIONS --
+-- ===============================
 
+-- TRIGGERS --
+
+
+-- PROCEDURES --
+
+
+-- FUNCTIONS --
+DELIMITER //
+CREATE FUNCTION f_get_full_name(first_name VARCHAR(200), last_name VARCHAR(200))
+RETURNS VARCHAR(200)
+DETERMINISTIC
+BEGIN
+	RETURN CONCAT(first_name, " ", last_name);
+END //
+DELIMITER ;
