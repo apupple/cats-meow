0058fd6e1baba8f389115a861c5480123cc053b5
apupple <kyleeannreid@gmail.com>
2026-04-10 14:19:31 -0500

    Update 04_triggers_procedure_function.sql
    New function for getting an address

--- 04_triggers_procedure_function.sql
+++ 04_triggers_procedure_function.sql
@@ -9,11 +9,22 @@
 
 
 -- FUNCTIONS --
+-- 1) Get Full Name: Combines first and last names for full name string --
 DELIMITER //
 CREATE FUNCTION f_get_full_name(first_name VARCHAR(200), last_name VARCHAR(200))
 RETURNS VARCHAR(200)
 DETERMINISTIC
 BEGIN
 	RETURN CONCAT(first_name, " ", last_name);
+END //
+DELIMITER ;
+
+-- 2) Get Address: Combines street, city, state, and zip code for full address string--
+DELIMITER //
+CREATE FUNCTION f_get_address(street VARCHAR(200), city VARCHAR(200), state VARCHAR(200), zipCode INT)
+RETURNS VARCHAR(200)
+DETERMINISTIC
+BEGIN
+	RETURN CONCAT(street, ", ", city, ", ", state, " ", zipCode);
 END //
 DELIMITER ;
