516dabee8b6578c4add83e523ef50db439130221
Sanjana Sathish <sanjanasathish565@gmail.com>
2026-04-10 00:02:43 -0500

    Insert multiple records into person table

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -2,4 +2,10 @@
 -- P.A.W.S. DATA --
 ===============================
 -- 1) PERSON --
-  
+INSERT INTO person VALUES
+  (1, Light, Yagami, light.yagami@gmail.com, 02/28/2004, Street 1, San Antonio, TX, 78249)
+  (2, Gojo, Satoru, imGojo@gmail.com, 12/07/2001, Street 2, San Antonio, TX, 78249)
+  (3, Tanjiro, Kamado, tanjiro.kamado@gmail.com, 03/18/2008, Street 3, San Antonio, TX, 78249)
+  (4, Langa, Hasegawa, langa.hasegawa@gmail.com, 07/12/2007, Street 4, San Antonio, TX, 78249)
+  (5, Nancy, Wheeler, notNancyDrew@gmail.com, 08/27/1999, Street 5, San Antonio, TX, 78249)
+  (6, Will, Byers, will.byers123@gmail.com, 06/15/2007, Street 6, San Antonio, TX, 78249)
