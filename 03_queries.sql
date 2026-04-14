7e779f1a3b707e29793726112f4afede54b8beb7
Sanjana Sathish <sanjanasathish565@gmail.com>
2026-04-13 20:13:02 -0500

    Add example outputs for SQL queries
    Added example outputs for SQL queries to clarify expected results.

--- 03_queries.sql
+++ 03_queries.sql
@@ -10,6 +10,8 @@ SELECT	p.person_id,
 FROM PAWS.person p
 LEFT JOIN PAWS.phone n ON p.person_ID = n.person_ID
 GROUP BY p.person_ID;
+-- ORDER OUTPUT: # person_id, name, phone_numbers, person_email, address
+-- EX OUTPUT: '1', 'Light Yagami', '210-123-4567', 'light.yagami@gmail.com', '1024 Shadow Lane, Los Angeles, CA 90012'
 
 -- 2) Adopter Information --
 SELECT
@@ -26,6 +28,8 @@ JOIN person p ON a.person_id = p.person_id
 JOIN phone ph ON p.person_id = ph.person_id
 LEFT JOIN rent_info ri ON a.rent_info_id = ri.rent_info_id
 ORDER BY p.person_last_name, p.person_first_name;
+-- ORDER OUTPUT: # full_name, person_email, phone_number, address, adopter_housing_info, adopter_is_fenced, is_rented, charge_of_rent
+-- EX OUTPUT: 'Barry Allen', 'barry.allen@gmail.com', '940-508-6930', '400 Speedster Dr, Cleveland, OH 44101', 'apartment with siblings', '0', '1', '800.00'
 
 -- 3) What species and breeds are currently in the shelter?
 SELECT DISTINCT
@@ -36,6 +40,8 @@ FROM breed b
 JOIN animal a ON b.breed_id = a.breed_id
 GROUP BY b.breed_species, b.breed_name
 ORDER BY b.breed_species, animals_in_shelter DESC;
+-- ORDER OUTPUT: # breed_species, breed_name, animals_in_shelter
+-- EX OUTPUT: 'Canis Lupus Familiaris', 'German Shepherd', '2'
 
 -- 4) How full is each enclosure?
 SELECT
@@ -47,6 +53,9 @@ SELECT
     ROUND(f_calculate_current_occupancy(enclosure_id) / enclosure_max_capacity * 100, 1) AS pct_full
 FROM enclosure
 ORDER BY pct_full DESC;
+-- ORDER OUTPUT: # enclosure_id, enclosure_sanitation_status, enclosure_max_capacity, current_occupancy, available_spots, pct_full
+-- EX OUTPUT: '1', 'Clean', '12', '8', '4', '66.7'
+
 
 -- 5) Employee Payroll
 SELECT
@@ -68,8 +77,10 @@ WHERE e.employee_status = 'EMPLOYED'
   )
 GROUP BY e.employee_dept, p.person_first_name, p.person_last_name, e.employee_job_title
 ORDER BY avg_rate DESC;
+-- ORDER OUTPUT: employee_dept, staff_count, lowest_rate, highest_rate, avg_rate, top_earner, top_earner_title
+-- EX OUTPUT: 'adoption', '1', '20.00', '20.00', '20.00', 'Wednesday Addams', 'COORDINATOR'
 
--- 6) Volunteer Hours Leaderbaord
+-- 6) Volunteer Hours Leaderboard
 SELECT
     f_get_full_name(p.person_first_name, p.person_last_name) AS volunteer_name,
     v.volunteer_total_hours,
@@ -90,3 +101,6 @@ GROUP BY v.volunteer_id, p.person_first_name, p.person_last_name,
          v.volunteer_total_hours, v.volunteer_status,
          v.volunteer_training_complete, v.volunteer_start_date
 ORDER BY v.volunteer_total_hours DESC;
+-- ORDER OUTPUT: volunteer_name, volunteer_total_hours, volunteer_status, volunteer_training_complete, volunteer_start_date, preferred_tasks
+-- EX OUTPUT: 'Clark Kent', '20', 'ACTIVE', '1', '2025-09-20', 'playing with animals, walking dogs'
+
