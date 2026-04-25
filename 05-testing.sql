3867e9c34faea54ee318d11a2150161b30e82e0d
apupple <kyleeannreid@gmail.com>
2026-04-25 13:11:58 -0500

    Updated testing files

--- 05-testing.sql
+++ 05-testing.sql
@@ -1,1 +1,5 @@
-INSERT INTO animal VALUES (49, 1, 1, 49, 'Test Dog', 2,  'F', 'this dog is for testing!')
+INSERT INTO animal VALUES (49, 1, 1, 49, 'Test Dog', 2,  'F', 'this dog is for testing!');
+
+UPDATE animal SET enclosure_id = 1 WHERE animal_id = 3;
+
+DELETE FROM enclosure WHERE enclosure_id = 1;
